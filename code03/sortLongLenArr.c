62fa7641836c4c1d2d088fe78f2702163e1492c3
zjx <zjx@163.com>
2023-12-13 09:32:54 +0800

    [新增]新增原地作业的代码

---
+++ code03/sortLongLenArr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LENTH 8
+int main()
+{
+    int str[LENTH] = {1 , 2 , 3 , 4 , 1 , }
+
+
+
+    return 0;
+}
---
+++ code04/removeTarget.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// #define SIZE 8
+
+int main()
+{
+    int target;
+    scanf("%d",&target);
+    int size;
+    printf("输入数组大小：");
+    scanf("%d",&size);
+
+    // int *array = (int*)malloc(sizeof(int)*size);
+    int array[size];
+    int pos = 0;
+
+    srand(time(NULL));
+    for(int idx = 0; idx < size; idx++)
+    {
+        array[idx] = rand() % 10 + 1;//随机1-10
+        printf("%d\t", array[idx]);
+    }
+    printf("\n");
+
+    for(int idx = 0; idx < size; idx++)
+    {
+        if(array[idx] != target)
+        {
+            //原地 空间复杂度0（1）
+            array[pos] = array[idx];
+            pos++;
+            printf("%d ", array[pos]);          
+        }
+    }
+    printf("\n");
+    
+
+
+
+
+    // srand(time(NULL));
+    // int arr1[SIZE];
+    // memset(arr, 0, SIZE);
+
+    // for (int idx = 0; idx < SIZE; idx++)
+    // {
+    //     arr1[idx] = rand() % 10 + 1;
+    //     printf("%d\t", arr1[idx]);
+    // }
+    // printf("over\n");
+
+    // int target = rand() % 10 + 1;
+    // printf("target:%d\n", target);
+
+    
+
+    // for(int idx = 0; idx < SIZE; idx++)
+    // {
+    //     if(target != arr[idx])
+    //     {
+            
+    //     }
+    // }
+    return 0;
+}
