002305445bc522d8ff70f587f37f63011c730e19
zjx <zjx@163.com>
2023-12-18 13:51:33 +0800

    [新增]新增作业：单词倒置

--- leedcode09/wordReverse.c
+++ leedcode09/wordReverse.c
@@ -20,11 +20,30 @@ void reverse(char *l, char *r)
 
 int main()
 {
-    char arr[] = " I am   from China";
+    char arr[] = "  I am   from China";
     // char arr[100] = {0};
     // scanf("%[^\n]", arr);
     // fgets(arr, sizeof(arr), stdin);
+
     int len = strlen(arr);
+    int pos = 0;
+    for (int idx = 0; idx < len; idx++)
+    {
+        if (arr[idx] != ' ')
+        {
+            arr[pos] = arr[idx];
+            pos++;
+        }
+        if (arr[idx] != ' ' && arr[idx + 1] == ' ')
+        {
+            arr[pos] = arr[idx + 1];
+            pos++;
+        }
+        arr[pos] = '\0';
+    }
+    printf("%s\n", arr);
+
+    len = strlen(arr);
     char *left = arr;
     char *right = arr + len - 1;
     reverse(left, right);
@@ -32,35 +51,21 @@ int main()
     
     char *start = arr;
     char *current = arr;
-    int count = 0;
-    while (current != '\0')
+    while (*current != '\0')
     {
-        for (int idx = 0; idx < len; idx++)
+        while (*current != ' ' && *current != '\0')
         {
-            while (*current != '\0')
-            {
-                while (*start = ' ')
-                {
-                    start++;
-                }
-                while (*current != ' ' && *current != '\0')
-                {
-                    current++;
-                }
-                reverse(start, current)
-            }
+            current++;
         }
-        
-        while (*current != '\0' && *current != '\0')
+        reverse(start, current - 1);
+        start = current + 1;
+        if (*current != '\0')
         {
+            current++;
         }
-
-        reverse(start, arr[idx + ])
     }
-    
-   
-    
-
+    // arr[*current] = '\0';
+    printf("%s\n", arr);
 
     return 0;
 }
